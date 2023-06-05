6a23441d17295620762d338b9c6d18c8e0094aef
Damon McCullough <damonmcc1391@gmail.com>
2023-06-05 09:08:49 -0400

    better notes

--- bash/setup_environment.sh
+++ bash/setup_environment.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 #
 # Dev script to compile python packages from a requirements.in file to a requirements.txt file.
+# Expects a virtual environment named .venv to already exist in the parent directory
+
 set -e
 RELATIVE_SCRIPTPATH=$0
-
-# creating virtual environment
-# TODO check if it already exists first
 
 # activate virtual environment
 echo "~~~ Activating virtual environment ~~~"
 source .venv/bin/activate
-which python
-python --version
+echo "Active python version: $(python --version)"
+echo "Active python interpreter: $(which python)"
 
 # Update and install packages used to compile requirements
 echo "~~~ Installing build packages ~~~"
